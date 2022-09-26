3bb5129803181c56c0e00dd1b5c4273d0a5e77da
tigerBR0 <khurbanovrakhmiddin@gmail.com>
2022-09-27 01:24:04 +0500

    4 add custom tabb bar and provider

---
+++ lib/pages/history/history_page.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/material.dart';class HistoryPage extends StatelessWidget {  const HistoryPage({Key? key}) : super(key: key);  @override  Widget build(BuildContext context) {    return Scaffold();  }}
